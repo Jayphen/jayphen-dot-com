d46fa2bead2d992c848663c47276aedb0e96f21f
Jayphen <jayphen@gmail.com>
2017-04-20 00:40:31 +0200

    Add current location from foursquare

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,4 +1,5 @@
 //= require 'jquery/dist/jquery.js'
+//= require 'jsrender/jsrender.min.js'
 
 var counter = 0,
     counterEl = $('.js-counter');
@@ -6,3 +7,19 @@ setInterval(function(){
   ++counter;
   counterEl.text(counter < 300 ? " " + counter + " " : " dickity 3 score ");
 }, 1000);
+
+var config = {
+    apiKey: "AIzaSyBudjiXVSYk5xbCOcBHiPRte6qhFt9C64w",
+    databaseURL: "https://personal-site-2b705.firebaseio.com",
+};
+
+var $flag = $('.logoish img');
+
+firebase.initializeApp(config);
+var database = firebase.database();
+firebase.database().ref('/location').on('value', function(snapshot) {
+  var tmpl = $.templates("#myTmpl");
+  var data = snapshot.val();
+  var html = tmpl.render(data); 
+  $("#location").html(html);
+});
