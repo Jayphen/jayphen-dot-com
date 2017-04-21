6b25feccdf3fcdf244989f473039a7a0c7350cef
Jayphen <jayphen@gmail.com>
2017-04-21 19:41:11 +0200

    Larger instagram images

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -9,7 +9,7 @@ var feed = new Instafeed({
   get: 'user',
   userId: '308943',
   limit: 7,
-  // resolution: 'low_resolution',
+  resolution: 'low_resolution',
   accessToken: '308943.a96bcb0.71fcf01f0899437caa734c55b4fedeb9',
   template: '<a href="{{link}}"><img src="{{image}}" /></a>'
 });
