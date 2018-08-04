eb16151a31cd68afd14fbd396f66b4cff768e5af
Jayphen <jayphen@gmail.com>
2018-08-04 14:03:08 +0200

    Remove instagram feed

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -2,15 +2,3 @@
 //= require './_dumb_counter'
 //= require './_get_location'
 //= require './_get_status'
-
-//= require 'instafeed.js/instafeed.js'
-
-var feed = new Instafeed({
-  get: 'user',
-  userId: '308943',
-  limit: 7,
-  resolution: 'low_resolution',
-  accessToken: '308943.a96bcb0.dfc57fc3d06a496b810284a48fd85f01',
-  template: '<a href="{{link}}"><img src="{{image}}" /></a>'
-});
-feed.run();
